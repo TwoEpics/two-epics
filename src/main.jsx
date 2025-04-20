2af67e333de96532d596eb9bd7550de3a2fe9be3
Jeramy Terry <jeramy.terry@twoepics.com>
2025-04-20 10:49:44 -0400

    Added blog system

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,1 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-
-const App = () => (
-  <div className="text-center text-2xl font-bold mt-10 text-gray-800">
-    Welcome to Two Epics 🌿
-  </div>
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// React entry point
