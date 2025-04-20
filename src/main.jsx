4a3d3c93be3385a8c9e6716e056a0997de65f7e8
Jeramy Terry <jeramy.terry@twoepics.com>
2025-04-20 09:10:31 -0400

    Clean working build of Two Epics

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const App = () => (
+  <div className="text-center text-2xl font-bold mt-10 text-gray-800">
+    Welcome to Two Epics 🌿
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
